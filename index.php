2528d06187bf2efe4d7ce439887cb3aef77f29ca
ghost <bloodygwin@yahoo.com>
2018-04-13 21:17:51 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link type="text/css" rel="stylesheet" href="styles/style.css">
+	<title>My website</title>
+</head>
+
+<body>
+<div class="main-container">
+	<div class="main-wrapper">
+		<!-- this is the first header -->
+		<div class="first-text">
+			<p>Hello HNG user!</p>
+		</div>
+		<div class="second-text">
+			<h2>FIRST DESIGN USING FIGMA</h2>
+		</div>
+		
+	<!-- this div hold two col -->
+	<div class="content clearfix">
+	<div id="left">
+		<img src="styles/images/icons8-last-24-hours-24.png">
+		<h2>SESSION RUN TIME</h2>
+		<p>one step at a time the journey:</p>
+	</div>
+	<div id="right">
+		<p><?PHP echo date("h : i a") ?></p>
+	</div>
+	</div>
+	<div class="buttom-container">
+		<h3>SET THE NEW<br>TIME<br>NOW IT IS FREE</h3>
+	</div>	
+		
+	</div>
+</div>
+
+</body>
+
+</html>
